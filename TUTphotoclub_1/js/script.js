7f7c91d548e5409137a5323775c7b592e598f0bb
makkimakky <m011530928@edu.teu.ac.jp>
2018-02-14 12:05:19 +0900

    てｓってｓｔ

--- TUTphotoclub_1/js/script.js
+++ TUTphotoclub_1/js/script.js
@@ -15,15 +15,3 @@ $(function($) {
         easing: "swing",  // 切り替える背景画像を指定
     });
 });// http://bashalog.c-brains.jp/14/07/17-095900.php
-
-
-
-// $(window).resize(function(){
-//     var x = $(window).width();
-//     var y = 640;
-//     if (x <= y) {
-//         $('body').removeClass('bg-slider');
-//     }else{
-//         $('body').addClass('bg-slider')
-//     }
-// });
