93528e6291082682f6ceba882e98c9227f525d5e
makkimakky <m011530928@edu.teu.ac.jp>
2018-02-14 11:50:20 +0900

    もいちょ

--- TUTphotoclub_1/js/script.js
+++ TUTphotoclub_1/js/script.js
@@ -16,12 +16,7 @@ $(function($) {
     });
 });// http://bashalog.c-brains.jp/14/07/17-095900.php
 
-$(function($) {
-    $('.bg-slider_top').bgSwitcher({
-        images: ['img/_DSC0941.JPG','img/_DSC1009.JPG','img/_DSC1549.JPG'],
-        effect: "fade",
-        easing: "swing",  // 切り替える背景画像を指定
-    });
+
 });// http://bashalog.c-brains.jp/14/07/17-095900.php
 
 // $(window).resize(function(){
