502b6447495dd3935fd7b73345bfb71c90b9a341
makkimakky <m011530928@edu.teu.ac.jp>
2018-03-17 00:37:44 +0900

    忘れてたところ修正

--- makki_portfolio/js/script.js
+++ makki_portfolio/js/script.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
       $('#menu').slideToggle();
     });
   });
+
+  //ページ頭に戻る処理
+  $(document).ready(function() {
+      $("#page-top a").click(function() {
+        $('html,body').animate({
+            scrollTop: 0
+          }, '2200');
+        return false;
+      });
+    });
